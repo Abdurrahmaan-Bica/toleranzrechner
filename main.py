c4fc83d36cdfc235f6724e21f92a0cb349d0f01d
codelove <abdurrahmaanbica123@gmail.com>
2024-11-11 14:37:56 +0100

    Hinweis of Willkommenstext changed. 2 New methods for the main menu were added

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ willkommens_text = Figlet()
 print(willkommens_text.renderText("Python Toleranzrechner"))
 print("======================================================================")
 print("Willkommen zum Toleranzrechner!")
-print("Bitte beachten Sie: Dieser Rechner wurde getestet, allerdings kann man nicht die Genauigkeit der Ergebnisse gewährleisten.")
+print("Hinweis: Dieser Rechner wurde getestet, aber die Genauigkeit der Ergebnisse kann nicht garantiert werden.")
 print("======================================================================")
+# Methode für die Hauptmenü
+def haupt_menu_anzeigen():
+    print()
+    print("Was möchten Sie tun?" + "\n" + "1. Berechnung starten" + "\n" + "2. Programm beenden\n")
+# Methode für die verschiedenen Toleranzklassen
+def toleranz_menu_anzeigen():
+    print()
+    print("Wählen Sie bitte die gewünschte Toleranzklasse aus:" + "\n" + "1. H6" + "\n" + "2. H8" + "\n" + "3. g6" + "\n" + "4. r6" + "\n" + "5. Zurück zum Hauptmenü\n")
