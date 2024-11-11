18ae77d15b509a54c55a261e94e33da16fc7eb09
codelove <abdurrahmaanbica123@gmail.com>
2024-11-11 14:43:14 +0100

    method to check the inserted nennmass was added. Explanation of the method was also added.

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ print("======================================================================")
 print("Willkommen zum Toleranzrechner!")
 print("Hinweis: Dieser Rechner wurde getestet, aber die Genauigkeit der Ergebnisse kann nicht garantiert werden.")
 print("======================================================================")
+
 # Methode für die Hauptmenü
 def haupt_menu_anzeigen():
     print()
     print("Was möchten Sie tun?" + "\n" + "1. Berechnung starten" + "\n" + "2. Programm beenden\n")
+
 # Methode für die verschiedenen Toleranzklassen
 def toleranz_menu_anzeigen():
     print()
     print("Wählen Sie bitte die gewünschte Toleranzklasse aus:" + "\n" + "1. H6" + "\n" + "2. H8" + "\n" + "3. g6" + "\n" + "4. r6" + "\n" + "5. Zurück zum Hauptmenü\n")
+
+# Die folgende Methode überprüft, ob das eingegebene Nennmaß innerhalb des gültigens Intervalls ist. [1-30] mm.
+# Es gibt 2 Rückgabewerte:
+# Falls das Nennmaß richtig ist, ist das Nennmaß der Rückgabewert.
+# Falls das Nennmaß nicht innerhalb des Intervalls ist, wird eine Informationsmeldung angezeigt und nichts zurückgegeben.
+def nennmass_ueberpruefen(nennmass):
+    if nennmass >= 1 and nennmass <= 30:
+        return nennmass
+    else:
+        print(f"Ungültiges Nennmaß!Das eingegebene Nennmaß von {nennmass} mm ist außerhalb des gültigen Intervalls [1-30].")
+        return None
