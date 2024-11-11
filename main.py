56fb8c9fc67a945f3c267e4800f35910b1e3ff02
codelove <abdurrahmaanbica123@gmail.com>
2024-11-11 14:32:46 +0100

    Willkommenstext added

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from pyfiglet import Figlet
+from tabulate import tabulate
 
+willkommens_text = Figlet()
+print(willkommens_text.renderText("Python Toleranzrechner"))
+print("======================================================================")
+print("Willkommen zum Toleranzrechner!")
+print("Bitte beachten Sie: Dieser Rechner wurde getestet, allerdings kann man nicht die Genauigkeit der Ergebnisse gewährleisten.")
+print("======================================================================")
